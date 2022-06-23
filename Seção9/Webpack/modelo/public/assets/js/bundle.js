1d027a6a5a987ea62de941aa72e4dd6bbcb920d4
lucaskenp <lucasmonteirolima17@gmail.com>
2022-06-22 22:26:52 -0300

    webpack

---
+++ Seção9/Webpack/modelo/public/assets/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//
+//
+//
+//
+console.log('Olá');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
