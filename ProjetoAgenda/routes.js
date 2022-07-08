815b86f3af82af03021560bc20f105b4807800d7
lucaskenp <lucasmonteirolima17@gmail.com>
2022-07-07 21:04:00 -0300

    Logando usuário

--- ProjetoAgenda/routes.js
+++ ProjetoAgenda/routes.js
@@ -10,5 +10,7 @@ route.get('/', homeController.paginaInicial);
 
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
 
 module.exports = route;
