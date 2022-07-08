db989120b4d44f7185072afc3c19d71548570c8f
lucaskenp <lucasmonteirolima17@gmail.com>
2022-07-08 20:27:02 -0300

    Cadastrando contatos(Create)

--- ProjetoAgenda/routes.js
+++ ProjetoAgenda/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const route = express.Router();
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
 const homeController = require('./src/controllers/homeController');
+
+const { loginRequired } = require('./src/middlewares/middleware');
 
 // Rotas da home
 route.get('/', homeController.paginaInicial);
@@ -12,5 +15,12 @@ route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+// Rotas de Contato
+
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+
 
 module.exports = route;
