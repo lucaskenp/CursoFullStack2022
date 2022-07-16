9b456e3015e5a8b76d3353e671334280304c9600
lucaskenp <lucasmonteirolima17@gmail.com>
2022-07-16 15:11:53 -0300

    Configurando e testando sequelize

--- API_REST/src/controllers/HomeController.js
+++ API_REST/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'João',
+      sobrenome: 'Lucas',
+      email: 'lucasmonteirolima17@gmail.com',
+      idade: 21,
+      peso: 69,
+      altura: 1.72,
     });
+    res.json(novoAluno);
   }
 }
 
