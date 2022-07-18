5777dd03b1e68aed5bb59342ad1f8c1cf599396b
lucaskenp <lucasmonteirolima17@gmail.com>
2022-07-18 14:37:51 -0300

    Rotas de alunos

--- API_REST/src/controllers/HomeController.js
+++ API_REST/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'João',
-      sobrenome: 'Lucas',
-      email: 'lucasmonteirolima17@gmail.com',
-      idade: 21,
-      peso: 69,
-      altura: 1.72,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
