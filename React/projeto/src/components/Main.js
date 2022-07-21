3aa16c3b96b93edb18463bc5d516ab9216c8c54e
lucaskenp <lucasmonteirolima17@gmail.com>
2022-07-20 21:01:27 -0300

    Estilizando tarefas

--- React/projeto/src/components/Main.js
+++ React/projeto/src/components/Main.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './Main.css';
-import { FaPlus } from 'react-icons/fa';
+
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: ['Fazer café', 'Estudar', 'Beber água'],
   };
 
   handleChange = (e) => {
@@ -14,7 +16,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -25,6 +27,18 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
