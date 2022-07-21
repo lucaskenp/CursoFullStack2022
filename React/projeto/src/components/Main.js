ce1a1846abd98d8b1279eab88361403e1a6729ed
lucaskenp <lucasmonteirolima17@gmail.com>
2022-07-21 11:31:33 -0300

    Salvando no localStorage

--- React/projeto/src/components/Main.js
+++ React/projeto/src/components/Main.js
@@ -10,6 +10,22 @@ export default class Main extends Component {
     index: -1,
   };
 
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (!tarefas) return;
+
+    this.setState({ tarefas });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+
+    if (tarefas === prevState.tarefas) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }
+
   handleEdit = (e, index) => {
     const { tarefas } = this.state;
     this.setState({
