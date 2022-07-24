5344dfff8e481b57a986e8c37f16a31852fa1b0f
lucaskenp <lucasmonteirolima17@gmail.com>
2022-07-24 14:51:55 -0300

    Router e History

--- React/react-base/src/routes/index.js
+++ React/react-base/src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes as Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
@@ -7,8 +7,8 @@ import Page404 from '../pages/Page404';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" element={<Login />} />
-      <Route path="*" element={<Page404 />} />
+      <Route exact path="/" component={Login} />
+      <Route path="*" component={Page404} />
     </Switch>
   );
 }
