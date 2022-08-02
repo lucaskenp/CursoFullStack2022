f6bb45c17970da7b2dd28fa5dec965a4768dd837
lucaskenp <lucasmonteirolima17@gmail.com>
2022-08-02 20:55:02 -0300

    Redux Persist

--- React/react-base/src/store/index.js
+++ React/react-base/src/store/index.js
@@ -1,16 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-
+import { persistStore } from 'redux-persist';
+import persistedReducers from './modules/reduxPersist';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducers(rootReducer),
   middleware: [sagaMiddleware],
 });
 
 sagaMiddleware.run(rootSaga);
 
+export const persistor = persistStore(store);
 export default store;
