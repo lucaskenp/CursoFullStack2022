5d2caeda199e7255301059ab48b71c7280982c44
lucaskenp <lucasmonteirolima17@gmail.com>
2022-08-05 19:39:32 -0300

    Sistema de login

--- React/consumirapi/src/pages/Login/index.js
+++ React/consumirapi/src/pages/Login/index.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { useDispatch } from 'react-redux';
+import { get } from 'lodash';
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import * as actions from '../../store/modules/auth/actions';
 
-export default function Login() {
+export default function Login(props) {
+  const dispatch = useDispatch();
+
+  const prevPath = get(props, 'location.state.prevPath', '/');
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha inválida');
+    }
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('Email inválido');
+    }
+
+    if (formErrors) return;
+
+    dispatch(actions.loginRequest({ email, password, prevPath }));
+  }
+
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
   return (
     <Container>
       <h1>Login</h1>
+
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" value={email} onChange={handleEmail} />
+        <input type="password" value={password} onChange={handlePassword} />
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
